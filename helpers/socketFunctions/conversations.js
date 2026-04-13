d48912ea3050bdd56fc27c8930c9e2beed7dd7be
Tyler <insertcleverusernamehere999@gmail.com>
2026-04-13 11:01:39 -0400

    removed comment notation from function

--- helpers/socketFunctions/conversations.js
+++ helpers/socketFunctions/conversations.js
@@ -1,15 +1,16 @@
-// const io = require("../../socket");
-// const users = require("../../socketStore");
+const { getIO } = require("../../socket");
+const users = require("../../socketStore");
 
-// const addConversationSocket = (conversation, to) => {
-//   if (users.has(to)) {
-//     const recipientUID = users.get(to).id;
-//     if (recipientUID) {
-//       io.to(recipientUID).emit("addConversation", {
-//         conversation,
-//       });
-//     }
-//   }
-// };
+const addConversationSocket = (conversation, to) => {
+  const io = getIO();
+  if (users.has(to)) {
+    const recipientUID = users.get(to).id;
+    if (recipientUID) {
+      io.to(recipientUID).emit("addConversation", {
+        conversation,
+      });
+    }
+  }
+};
 
-// module.exports = { addConversationSocket };
+module.exports = { addConversationSocket };
